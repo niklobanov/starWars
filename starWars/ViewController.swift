f76d2e2d3249b46b2067b6e0d07aabb5328ce46e
niklobanov <geenndn@Yandex.ru>
2019-04-03 11:24:23 +0300

    First commit

---
+++ starWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starWars
+//
+//  Created by Никита on 03/04/2019.
+//  Copyright © 2019 Никита. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
